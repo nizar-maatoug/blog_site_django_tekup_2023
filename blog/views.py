3055a059e58d44474bb39e7fa5eb591a505a670a
nizar-maatoug <nizar.maatoug@hotmail.com>
2023-11-02 18:33:13 +0100

    post seo-freindly

--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,7 @@ def post_list(request):
     print(list(posts))
     return render(request,'blog/post/list.html',{'posts':posts})
 
-def post_detail(request,id):
+def post_detail_1(request,id):
     try:
         post=Post.objects.get(id=id)
     except Post.DoesNotExist:
@@ -19,8 +19,13 @@ def post_detail(request,id):
     
     return render(request, 'blog/post/detail.html',{'post':post})
 
-def post_detail_1(request,id):
-    post=get_object_or_404(Post,id=id,status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     
     return render(request,'blog/post.detail.html',{'post':post})
 
