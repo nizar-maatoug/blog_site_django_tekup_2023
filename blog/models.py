714812bd409e12a1bc278ec6879520c9cb9c101a
nizar-maatoug <nizar.maatoug@hotmail.com>
2023-11-02 18:15:08 +0100

    post: url canonic

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+from django.urls import reverse
 
 # Create your models here.
 class PublishedManager(models.Manager):
@@ -37,6 +38,9 @@ class Post(models.Model):
         indexes = [
             models.Index(fields=['-publish']),
         ]
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',args=[self.id])
 
     def __str__(self):
         return self.title
